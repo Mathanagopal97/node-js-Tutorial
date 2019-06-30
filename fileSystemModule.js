e57d546964b233df863be3af420942fc9797ac0d
Mathanagopal Sankarasubramanian <mathug123@gmail.com>
2019-06-30 15:37:28 +0530

    Included Read function

--- fileSystemModule.js
+++ fileSystemModule.js
@@ -4,6 +4,14 @@ const fileSystemModule =  require('fs');
 fileSystemModule.writeFile('example.txt',"This is an example file",(err)=>{
     if(err)
         console.log(err);
-    else
+    else{
         console.log('file created successully');
+        fileSystemModule.readFile('example.txt','utf8',(err,fileContents)=>{
+            if(err)
+                console.log(err);
+            else
+                console.log(fileContents);
+        });
+    }
+        
 });
