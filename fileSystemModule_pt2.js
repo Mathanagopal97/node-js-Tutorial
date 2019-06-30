4c334f3fb59275dd52f36ca744925a199767bba9
Mathanagopal Sankarasubramanian <mathug123@gmail.com>
2019-06-30 16:15:00 +0530

    Update fileSystemModule_pt2.js

--- fileSystemModule_pt2.js
+++ fileSystemModule_pt2.js
@@ -4,14 +4,54 @@ const fileSystemModule = require('fs');
 fileSystemModule.mkdir('SampleDir',(err)=>{
     if(err)
         console.log(err);
-    else
+    else{
         console.log('Directory created successfully. \n');
+        fileSystemModule.writeFile('./SampleDir/example.txt','Sample data for the file. ',(err)=>{
+            if(err)
+                console.log(err);
+            else
+                console.log('File created successfully. \n');
+        });
+    }
+        
 });
 
-//Remove the directory
+// Remove Directory with files in it
+fileSystemModule.unlink('./SampleDir/example.txt',(err)=>{
+    if(err)
+        console.log(err);
+    else{
+        fileSystemModule.rmdir('SampleDir',(err)=>{
+            if(err)
+                console.log(err);
+            else
+                console.log('Directory removed successfully. \n')
+        });
+    }
+});
+
+
+//Remove the empty directory. Can only be used on an empty folder without any files in it.
 fileSystemModule.rmdir('SampleDir',(err)=>{
     if(err)
         console.log(err);
     else
         console.log('Directory removed successfully. \n')
+});
+
+//Read a directory and delete the files in them.
+fileSystemModule.readdir('SampleDir',(error,fileList)=>{
+    if(error)
+        console.log(err);
+    else{
+        console.log(fileList);
+        for(let file of fileList){
+            fileSystemModule.unlink(`./SampleDir/${file}`,(error)=>{
+                if(error)
+                    console.log(error);
+                else
+                    console.log(`${file} deleted successfully. \n`);
+            });
+        }
+    }
 });
