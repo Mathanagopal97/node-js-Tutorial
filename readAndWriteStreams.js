fb1c3e233bb6b2a597dfe64e9537000beec08500
Mathanagopal Sankarasubramanian <mathug123@gmail.com>
2019-06-30 16:53:40 +0530

    update readAndWriteStreams

--- readAndWriteStreams.js
+++ readAndWriteStreams.js
@@ -3,6 +3,8 @@ const fileSystemModule = require('fs');
 const readStream = fileSystemModule.createReadStream('example.txt','utf8');
 const writeStream = fileSystemModule.createWriteStream('example2.txt');
 //We use these streams on large chunks of data
+
+
 readStream.on('data',(chunkOfData)=>{
     console.log(chunkOfData);
     writeStream.write(chunkOfData,(error)=>{
