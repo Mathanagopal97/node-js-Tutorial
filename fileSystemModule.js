86326bdbb4a6a8d650fceba78f10b889d2032924
Mathanagopal Sankarasubramanian <mathug123@gmail.com>
2019-06-30 15:49:26 +0530

    Added the delete function

--- fileSystemModule.js
+++ fileSystemModule.js
@@ -5,7 +5,7 @@ fileSystemModule.writeFile('example.txt',"This is an example file",(err)=>{
     if(err)
         console.log(err);
     else{
-        console.log('File created successully');
+        console.log('File created successully \n');
         fileSystemModule.readFile('example.txt','utf8',(err,fileContents)=>{
             if(err)
                 console.log(err);
@@ -22,5 +22,21 @@ fileSystemModule.rename('example.txt','example2.txt',(err)=>{
     if(err)
         console.log(err);
     else
-        console.log('Rename Successful')
+        console.log('Rename Successful \n')
+});
+
+//Append Data to the File
+fileSystemModule.appendFile('example2.txt','Some data that is being appended',(err)=>{
+    if(err)
+        console.log(err);
+    else
+        console.log('Data Appended Successfully \n');
+});
+
+//Unlink a file
+fileSystemModule.unlink('example2.txt',(err)=>{
+    if(err)
+        console.log(err);
+    else
+        console.log('Unlink Successful. \n')
 });
