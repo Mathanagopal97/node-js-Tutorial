f889bfa440c0dc03fee05296ad64af789c748ff8
Mathanagopal Sankarasubramanian <mathug123@gmail.com>
2019-06-30 15:31:57 +0530

    Code to Create File

--- fileSystemModule.js
+++ fileSystemModule.js
@@ -1,1 +1,9 @@
 const fileSystemModule =  require('fs');
+
+//create file
+fileSystemModule.writeFile('example.txt',"This is an example file",(err)=>{
+    if(err)
+        console.log(err);
+    else
+        console.log('file created successully');
+});
