f5bf8dbf14352280dc4085d66bcaffaf9d441dc6
Mathanagopal Sankarasubramanian <mathug123@gmail.com>
2019-06-30 17:35:30 +0530

    Serving Static Files with Http and File System Module (html,json,image)

--- servingStaticFiles.js
+++ servingStaticFiles.js
@@ -1,6 +1,8 @@
 const httpModule = require('http');
 const fileSystemModule = require('fs');
 
+// 'image/png' and 'application.json'
+
 httpModule.createServer((request,response)=>{
     const readStream = fileSystemModule.createReadStream('./static/index.html');
     response.writeHead(200,{'Content-type' : 'text/html'});
